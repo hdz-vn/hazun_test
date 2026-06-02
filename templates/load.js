b0f1a2802810d5b5360d898dd98d5a647945c61e
Hazun06-VN <haphantridungqsqn@gmail.com>
2026-06-02 10:28:01 +0700

    Create load.js

---
+++ templates/load.js
@@ -0,0 +1,16 @@
+// Hàm tải template
+function loadTemplate(id, fileName) {
+    fetch(`/hazun_test/Templates/${fileName}`)
+        .then(res => res.ok ? res.text() : Promise.reject(`Lỗi tải ${fileName}`))
+        .then(html => document.getElementById(id).innerHTML = html)
+        .catch(err => console.error(err));
+}
+
+// Tự động chạy khi trang tải xong
+window.addEventListener('DOMContentLoaded', () => {
+    loadTemplate('security-place', 'security.html');
+    loadTemplate('menu-place', 'menu.html');
+    loadTemplate('clock-place', 'clock.html');
+    loadTemplate('footer-place', 'footer.html');
+    loadTemplate('chat-place', 'chat.html');
+});
